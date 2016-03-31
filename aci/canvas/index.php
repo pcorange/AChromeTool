f0f6e5d836a05535ebe2271faa90bf6a49d5ace3
linpcorange <471378935@qq.com>
2016-03-31 13:10:33 +0800

    test

---
+++ aci/canvas/index.php
@@ -0,0 +1,10 @@
+<html>
+<head>
+	<meta charset="utf-8">
+	<link rel="stylesheet" type="text/css" href="canvas.css">
+</head>
+<body>
+	<canvas width="1366" height="500" id="canvas">not support</canvas>
+	<img id="logo" src="logo.png">
+	<script type="text/javascript" src="canvas.js"></script>
+</body>
